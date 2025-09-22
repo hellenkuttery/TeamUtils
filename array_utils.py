a27ac11ea08b08ce612437a089852e69874084ce
kadir <kadir19kadir80@gmail.com>
2025-09-22 08:57:54 +0200

    add min_list, max_list, sum_list and average_list functions

---
+++ array_utils.py
@@ -0,0 +1,12 @@
+def sum_list(lst):
+    return sum(lst)
+
+def max_list(lst):
+    return max(lst)
+
+def min_list(lst):
+    return min(lst)
+
+def average_list(lst):
+    return sum(lst) / len(lst)
+
